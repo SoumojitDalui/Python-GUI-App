6f2d465cca09d64ca473204ab3485c578d8c2090
SoumojitDalui <39549918+SoumojitDalui@users.noreply.github.com>
2020-01-10 21:33:49 +0530

    Near Final

---
+++ App.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root=tk.Tk()
+apps=[]
+
+def addApp():
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    fileName=filedialog.askopenfile(initialdir="/Applications",title="Select File",filetypes=(("app","*.app"),("all files","*.*"))
+
+    apps.append(fileName)
+    print(fileName)
+
+    for app in apps:
+        label=tk.Label(frame,Text=app)
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+canvas=tk.Canvas(root,height=700,width=700, bg="#63B82C")
+canvas.pack()
+
+frame=tk.Frame(root,bg="white")
+frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+
+openFile=tk.Button(root,text="Open File",padx=320,pady=5,bg="#B8B23B",command=addApp)
+openFile.pack()
+
+runApps=tk.Button(root,text="Run the Applications",padx=288,pady=5,bg="#B8B23B",command=runApps)
+runApps.pack()
+
+root.update()
+
+root.mainloop()
